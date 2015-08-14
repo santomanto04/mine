6c5aea368e245fc44f008d7eb746efcc1076630e
santoshpradhan04 <daastanp@gmail.com>
2015-08-14 11:15:16 -0500

    Update p3_1001100648.s

--- p3_1001100648.s
+++ p3_1001100648.s
@@ -1,8 +1,8 @@
 /******************************************************************************
-* @FILE p2_1001100648.s
+* @FILE p3_1001100648.s
 * 
-* Simple program to implement an iterative solution for computing the GCD of 
-* two positive integers
+* Simple program to implement Euclid's algorithms for computing the GCD of 
+* two positive integers recursively
 *
 * @AUTHOR Santosh Pradhan
 ******************************************************************************/    
@@ -16,7 +16,7 @@ main:
     MOV R6, R0                          @ move return value from R0 to R6
     MOV R1, R5                          @ move R5 to R1
     MOV R2, R6                          @ move R6 to R2
-    BL GCD_EUCLID                    @ branch to GCD_ITERATIVE procedure with return
+    BL GCD_EUCLID                       @ branch to GCD_ITERATIVE procedure with return
     MOV R1, R0                          @ move R0 to R1
     LDR R0, =Printf_Output              @ R0 contains formatted string address
     BL printf                           @ call printf
@@ -35,30 +35,31 @@ _Operand:
     MOV PC, R4                          @ return
     
 GCD_EUCLID:
-    PUSH {LR}
-    CMP R2, #0
-    MOVEQ R0, R1
-    POPEQ {PC}
+    PUSH {LR}                           @ store the return address
+    CMP R2, #0                          @ compare the input argument to 0
+    MOVEQ R0, R1                        @ set return value to R1 if equal
+    POPEQ {PC}                          @ restore stack pointer and return if equal
     
-    CMP R1, R2
-    MOVHS R3, #1
-    CMP R2, #0
-    MOVGT R4, #1
-    B _modcheckloop
-    _modloop:
-          SUB R1, R1, R2
-    _modcheckloop:
-          CMP R1, R2
-          BHS _modloop
-          MOV R0, R1
+    CMP R1, R2                          @ check to see if R1 >= R2
+    MOVHS R4, #1                        @ swap R4 and 1 if 1 > R4
+    CMP R2, #0                          @ compare the input argument to 0
+    MOVGT R5, #1                        @ overrite R5 with 1 if 1 > R5
+    B _modCheck                         @ call _modCheck
     
-    MOV R1, R2
-    MOV R2, R0
-    CMP R3, R4
-    MOV R3, #0
-    MOV R4, #0
-    BEQ GCD_EUCLID
-    POP {PC}
+    _mod1:
+          SUB R1, R1, R2                @ subtract R2 from R1 and copy to R1
+    _modCheck:
+          CMP R1, R2                    @ check to see if R1 >= R7
+          BHS _mod1                     @ continue loop if R1 >= R7
+          MOV R0, R1                    @ move R1 to R0
+    
+    MOV R1, R2                          @ copy input register R2 to input register R1
+    MOV R2, R0                          @ copy return register R0 to input register R2
+    CMP R4, R5                          @ check to see if R4 >= R5
+    MOV R4, #0                          @ set a constant value for printing
+    MOV R5, #0                          @ set a constant value for printing
+    BEQ GCD_EUCLID                      @ branch to equal handler
+    POP {PC}                            @ restore the stack pointer and return
     
     
     .data
